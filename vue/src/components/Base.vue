88259d5ed0d79bff37303c032bdec69101a8b19c
梁亚宾 <liangyabin@datafountain.cn>
2020-06-05 10:14:20 +0800

    格式化

--- vue/src/components/Base.vue
+++ vue/src/components/Base.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="content">
-    <h1> 动漫回忆录 </h1>
+    <h1>动漫回忆录</h1>
     <ul>
       <li v-for="anime in animes" :key="anime.get('id')">
-        <h3>《{{anime.get('title')}}》 观影年龄：{{anime.get('age')}} </h3>
-          
+        <h3>《{{ anime.get('title') }}》 观影年龄：{{ anime.get('age') }}</h3>
+
         <i v-for="people in anime.get('peopleArr')" :key="people.get('id')">
-        「 {{people.get('name')}} 」
+          「 {{ people.get('name') }} 」
         </i>
 
-        <br v-if="anime.get('peopleArr')"/>
-        <span @click="updateAnime(anime)">修改</span> 
+        <br v-if="anime.get('peopleArr')" />
+        <span @click="updateAnime(anime)">修改</span>
         <span @click="removeAnime(anime)">删除</span>
         <span @click="addPeople(anime)">添加人物</span>
       </li>
@@ -20,91 +20,87 @@
 </template>
 
 <script>
-import AV from "leancloud-storage";
+import AV from 'leancloud-storage'
 
 export default {
-  name: "Base",
+  name: 'Base',
   data() {
     return {
-      animes: []
-    };
+      animes: [],
+    }
   },
   async mounted() {
-    this.getAnime();
+    this.getAnime()
   },
   methods: {
     async getAnime() {
-      const query = new AV.Query("Anime");
-      query.include(["peopleArr"]);
-      query.ascending("createdAt");
+      const query = new AV.Query('Anime')
+      query.include(['peopleArr'])
+      query.ascending('createdAt')
       try {
-        this.animes = await query.find();
+        this.animes = await query.find()
       } catch (error) {
-        alert(error);
+        alert(error)
       }
     },
     // 添加数据
     async saveAnime() {
-      const Anime = AV.Object.extend("Anime");
-      const anime = new Anime();
-      const title = prompt("动漫名字？", "");
-      const age = prompt("儿时年龄？", "");
-      anime.set("title", title);
-      anime.set("age", age);
-      try {
-        await anime.save();
-        this.getAnime();
-        alert("保存成功");
-      } catch (error) {
-        alert(error);
-      }
+      const Anime = AV.Object.extend('Anime')
+      const anime = new Anime()
+      const title = prompt('动漫名字？', '')
+      const age = prompt('儿时年龄？', '')
+      anime.set('title', title)
+      anime.set('age', age)
+      await anime.save()
+      // 更新页面
+      this.getAnime()
+      alert('保存成功')
     },
     async updateAnime(anime) {
-      const title = prompt("动漫名字？", "");
-      const age = prompt("几岁看的？", "");
-      anime.set("title", title);
-      anime.set("age", age);
+      const title = prompt('动漫名字？', '')
+      const age = prompt('几岁看的？', '')
+      anime.set('title', title)
+      anime.set('age', age)
       try {
-        await anime.save();
-        this.getAnime();
-        alert("保存成功");
+        await anime.save()
+        this.getAnime()
+        alert('保存成功')
       } catch (error) {
-        alert(error);
+        alert(error)
       }
     },
     async removeAnime(anime) {
-      if (!confirm("确定要删除吗？")) return;
+      if (!confirm('确定要删除吗？')) return
       try {
-        await anime.destroy();
-        this.getAnime();
+        await anime.destroy()
+        this.getAnime()
       } catch (error) {
-        alert(error);
+        alert(error)
       }
     },
     async addPeople(anime) {
-      const People = AV.Object.extend("People");
-      const people = new People();
-      const name = prompt("人物名字？", "");
-      people.set("name", name);
-      people.set("anime", anime);
+      const People = AV.Object.extend('People')
+      const people = new People()
+      const name = prompt('人物名字？', '')
+      people.set('name', name)
+      people.set('anime', anime)
       try {
-        await people.save();
+        await people.save()
       } catch (error) {
-        alert(error);
+        alert(error)
       }
 
       try {
-        anime.add("peopleArr", people);
-        await anime.save();
-        this.getAnime();
+        anime.add('peopleArr', people)
+        await anime.save()
+        this.getAnime()
       } catch (error) {
-        alert(error);
+        alert(error)
       }
-    }
-  }
-};
+    },
+  },
+}
 </script>
-
 
 <style scoped>
 .content {
@@ -145,11 +141,6 @@ li span {
 }
 </style>
 
-
-
-    // // 新建一个 ACL 实例
-    //   var acl = new AV.ACL();
-    //   acl.setPublicReadAccess(true);
-    //   acl.setWriteAccess(AV.User.current(), true);
-    //   // 将 ACL 实例赋予 anime 对象
-    //   anime.setACL(acl);
+// // 新建一个 ACL 实例 // var acl = new AV.ACL(); //
+acl.setPublicReadAccess(true); // acl.setWriteAccess(AV.User.current(), true);
+// // 将 ACL 实例赋予 anime 对象 // anime.setACL(acl);
